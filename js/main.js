2e3a2c308af803dad8fefa147416efa40e3f1bcf
Lautaro Roldan <lautaro-22@live.com.ar>
2023-04-05 10:22:35 -0300

    initialization

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const productos = [
+  { id: 1, nombre: "Milanesa", precio: 1199 },
+  { id: 2, nombre: "Hamburguesa", precio: 2199 },
+  { id: 3, nombre: "Fideos", precio: 1000 },
+  { id: 4, nombre: "Merluza", precio: 1420 },
+  { id: 5, nombre: "Papas Fritas", precio: 899 },
+  { id: 6, nombre: "Arroz", precio: 749 },
+  { id: 7, nombre: "Parrillada", precio: 3999 },
+  { id: 8, nombre: "Ensalada", precio: 1000 },
+];
+
+const integrantes = [
+  "Lautaro",
+  "Matias",
+  "Mariano",
+  "Julian",
+  "Nicolas",
+  "Tobias",
+  "Leandro",
+  "Nahuel",
+  "Tomas",
+];
+
+//usar prompts
